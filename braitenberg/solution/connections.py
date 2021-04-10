de19da47e93d282fd27414642ee81dafb49e38e7
tanij <tanij@ethz.ch>
2021-04-09 22:26:55 -0400

    JT review v2

--- braitenberg/solution/connections.py
+++ braitenberg/solution/connections.py
@@ -4,12 +4,12 @@ import numpy as np
 
 
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    res = np.zeros(shape=shape, dtype="float32")
+    res = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
 
     return res
 
 
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    res = np.zeros(shape=shape, dtype="float32")
+    res = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
 
     return res
